1f5f490602bfcd2d0bc72091fc07b696c7b53355
AnandTX <anand.aggarwal@testingxperts.com>
2022-02-20 00:31:00 +0530

    List of keywords

---
+++ src/firstProgram/KeywordList.py
@@ -0,0 +1,9 @@
+import keyword
+
+# Print the list as one word in one line
+# for item in keyword.kwlist:
+#     print(item)
+
+# printing all keywords at once using "kwlist()"
+print("The list of keywords is : ")
+print(keyword.kwlist)
