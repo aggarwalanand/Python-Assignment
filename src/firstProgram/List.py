d6b756c70f47de0b00187023feb0b7e68933f825
AnandTX <anand.aggarwal@testingxperts.com>
2022-02-24 22:56:38 +0530

    List functions

--- src/firstProgram/List.py
+++ src/firstProgram/List.py
@@ -1,6 +1,10 @@
 # List
 print('----LIST----')
 var = [1, 'Anand', 2, 'Aggarwal', 3, 4, 5, 6]
+var2 = [7, 8, 9]
+var3 = var2 + var
+print(var3)
+print(var2 + var)
 print(type(var))
 print(var)
 print(var[1])
@@ -13,21 +17,23 @@ print(var)
 var.extend([8, 9])
 print(var)
 
+print('----INSERT----')
 var = [1, 2, 3]
 var.insert(0, 0)
 print(var.index(3))
 print(var)
 
+print('----REMOVE----')
 var = [1, 2, 3]
 var.remove(1)
 print(var)
 
+print('----CLEAR----')
 var = [1, 2, 3]
 var.clear()
 print(var)
 
+print('----POP----')
 var = [1, 2, 3]
 var.pop()
 print(var)
-
-print('\n')
